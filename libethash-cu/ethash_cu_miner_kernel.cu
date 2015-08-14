6cb57116e1b1c8a36741c009de3eb375835307b6
Jan Willem Penterman <janwillem.penterman@ddbgroup.nl>
2015-08-14 13:40:09 +0200

    getting rid of cvt

--- libethash-cu/ethash_cu_miner_kernel.cu
+++ libethash-cu/ethash_cu_miner_kernel.cu
@@ -4,6 +4,8 @@
 * thanks to trpuvot,djm34,sp,cbuchner for things i took from ccminer.
 */
 
+#define SHUFFLE_MIN_VER 350
+
 #include "ethash_cu_miner_kernel.h"
 #include "ethash_cu_miner_kernel_globals.h"
 #include "rotl64.cuh"
@@ -214,7 +216,7 @@ __device__ hash32_t final_hash(hash64_t const* init, hash32_t const* mix)
 		state[i] = 0;
 	}
 
-	keccak_f1600_block(state,4);
+	keccak_f1600_block(state,1);
 
 	// copy out
 	copy(hash.uint64s, state, 4);
@@ -227,7 +229,7 @@ typedef union
 	hash32_t mix;
 } compute_hash_share;
 
-#if __CUDA_ARCH__ >= 300
+#if __CUDA_ARCH__ >= SHUFFLE_MIN_VER
 __device__ uint64_t compute_hash_shuffle(
 	hash32_t const* g_header,
 	hash128_t const* g_dag,
@@ -239,23 +241,33 @@ __device__ uint64_t compute_hash_shuffle(
 	
 	copy(state, g_header->uint64s, 4);
 	state[4] = nonce;
-	state[5] = 0x0000000000000001;
+	state[5] = 0x0000000000000001ULL;
 	for (uint32_t i = 6; i < 25; i++)
 	{
 		state[i] = 0;
 	}
-	state[8] = 0x8000000000000000;
+	state[8] = 0x8000000000000000ULL;
 	keccak_f1600_block(state, 8);
 
 	// Threads work together in this phase in groups of 8.
-	uint64_t const thread_id = threadIdx.x & (THREADS_PER_HASH - 1);
-	const int start_lane = (threadIdx.x >> 3) << 3;
-	
-	const uint32_t mix_idx = (thread_id & 3); 
+	const int thread_id = threadIdx.x & (THREADS_PER_HASH - 1);
+	const int start_lane = threadIdx.x & ~(THREADS_PER_HASH - 1);
+	const int mix_idx = (thread_id & 3); 
+
 	uint4 mix;
 
 	uint32_t shuffle[16];
-	uint32_t * init = (uint32_t *)state;
+	//uint32_t * init = (uint32_t *)state;
+
+	uint32_t init[16];
+	UNPACK64(init[0], init[1], state[0]);
+	UNPACK64(init[2], init[3], state[1]);
+	UNPACK64(init[4], init[5], state[2]);
+	UNPACK64(init[6], init[7], state[3]);
+	UNPACK64(init[8], init[9], state[4]);
+	UNPACK64(init[10], init[11], state[5]);
+	UNPACK64(init[12], init[13], state[6]);
+	UNPACK64(init[14], init[15], state[7]);
 
 	for (int i = 0; i < THREADS_PER_HASH; i++)
 	{
@@ -289,9 +301,8 @@ __device__ uint64_t compute_hash_shuffle(
 			{
 				if (thread_id == t)
 				{
-					shuffle[0] = fnv(init0 ^ (a + b), ((uint32_t *)&mix)[b]) % d_dag_size;
+					shuffle[0] = fnv(init0 ^ (a + b), ((uint32_t *)&mix)[b]) % d_dag_size;;
 				}
-				
 				shuffle[0] = __shfl(shuffle[0], start_lane + t);
 
 				mix = fnv4(mix, g_dag[shuffle[0]].uint4s[thread_id]);			
@@ -306,6 +317,7 @@ __device__ uint64_t compute_hash_shuffle(
 			shuffle[j] = __shfl(thread_mix, start_lane + j);
 
 		if (i == thread_id) {	
+
 			//move mix into state:
 			PACK64(state[8],  shuffle[0], shuffle[1]);
 			PACK64(state[9],  shuffle[2], shuffle[3]);
@@ -316,10 +328,10 @@ __device__ uint64_t compute_hash_shuffle(
 	}
 
 	// keccak_256(keccak_512(header..nonce) .. mix);
-	state[12] = 0x0000000000000001;
+	state[12] = 0x0000000000000001ULL;
 	for (uint32_t i = 13; i < 25; i++)
 	{
-		state[i] = 0;
+		state[i] = 0ULL;
 	}
 	state[16] = 0x8000000000000000;
 	keccak_f1600_block(state, 1);
@@ -378,7 +390,7 @@ ethash_search(
 	
 	uint32_t const gid = blockIdx.x * blockDim.x + threadIdx.x;	
 	
-#if __CUDA_ARCH__ >= 300
+#if __CUDA_ARCH__ >= SHUFFLE_MIN_VER
 	uint64_t hash = compute_hash_shuffle(g_header, g_dag, start_nonce + gid);
 	if (SWAP64(hash) < target)
 	{
@@ -417,8 +429,11 @@ void run_ethash_search(
 	uint64_t target
 )
 {
+#if __CUDA_ARCH__ >= SHUFFLE_MIN_VER
 	ethash_search <<<blocks, threads, 0, stream >>>(g_output, g_header, g_dag, start_nonce, target);
-//	ethash_search <<<blocks, threads, (sizeof(compute_hash_share) * threads) / THREADS_PER_HASH, stream>>>(g_output, g_header, g_dag, start_nonce, target);
+#else
+	ethash_search <<<blocks, threads, (sizeof(compute_hash_share) * threads) / THREADS_PER_HASH, stream>>>(g_output, g_header, g_dag, start_nonce, target);
+#endif
 }
 
 cudaError set_constants(
